04fc15c0a7c02ebf912e02d1fc12c36a6181bd7d
MirnaMilad <mirnamilad0101@gmail.com>
2025-09-27 23:45:58 +0300

    fix: update footer spacing, navbar links, and home section styles for improved layout and navigation

--- src/app/layout/navbar/navbar.config.ts
+++ src/app/layout/navbar/navbar.config.ts
@@ -1,7 +1,7 @@
 import { NavItems } from './navbar.model';
 
 export const NAV_ITEMS: NavItems[] = [
-  { label: 'Home', link: '/' },
+  { label: 'Home', link: '/home' },
   { label: 'Projects', link: '/projects' },
   { label: 'Skills', link: '/skills' },
 ];
